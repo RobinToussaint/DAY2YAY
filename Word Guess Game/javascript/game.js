878e2d4d010cf21cd862ddd2a32a6a31e333fd46
RobinToussaint <39896055+RobinToussaint@users.noreply.github.com>
2018-06-30 23:55:09 -0400

    Add files via upload

---
+++ Word Guess Game/javascript/game.js
@@ -0,0 +1,21 @@
+(function) {
+    var ThaiFood
+    var JapaneseFood
+    var SoulFood
+    var IndianFood
+    var CaribbeanFood
+
+    var Typeofcusine;
+    var Typeofcusine = ["Thai Food","Japanese Food", "Soul Food","Indian Food","Caribbean Food"] ;
+           for (var i =0; i <Typeofcusine.length; i++); {
+               console.log("Typeofcusine" + Typeofcusine[i]);
+           }
+    
+           var Typeofcusine= ["Thai Food","Japanese Food", "Soul Food","Indian Food","Caribbean Food"];
+    var underScores = [];
+    for(var i =0; i < word.length; i++) {
+     underScores.push("- - - - - - - -");
+    } 
+
+}
+
